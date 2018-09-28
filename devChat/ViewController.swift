d97bd1233ea62f70915b0fca5dfcd9dbc6596b7a
Ivan Ramirez <ramirezfranciscoivan@gmail.com>
2018-09-28 14:18:14 -0600

    Initial Commit

---
+++ devChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  devChat
+//
+//  Created by Ivan Ramirez on 9/28/18.
+//  Copyright © 2018 ramcomw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
